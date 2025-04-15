af49507131a5ba8e94d3f0fb7a22820a67ad6175
D <gooole.dql@gmail.com>
2025-04-15 14:13:18 +0800

    添加延迟缓冲，修改ANIM_NUM保证不闪屏

--- main.cpp
+++ main.cpp
@@ -1,32 +1,62 @@
 #include <graphics.h>
+#include <string>
 
 int idx_cur_anim = 0;
+const int ANIM_NUM = 5;
+
+IMAGE img_player_left[ANIM_NUM];
+IMAGE img_player_right[ANIM_NUM];
+
+void loadanimation()
+{
+	for (size_t i = 0; i < ANIM_NUM; i++)
+	{
+		std::wstring path = L"img/player_left_"+std::to_wstring(i+1)+L".png";
+		loadimage(&img_player_left[i], path.c_str());
+	}
+    for (size_t i = 0; i < ANIM_NUM; i++)
+	{
+		std::wstring path = L"img/player_right_"+std::to_wstring(i+1)+L".png";
+		loadimage(&img_player_right[i], path.c_str());
+	}
+}
 
 int main()
 {
 	initgraph(1280, 720);
-
+	
 	bool running = true;
 
 	ExMessage msg;
 	IMAGE img_background;
 
 	loadimage(&img_background, _T("img/background.png"));
+	loadanimation();
 
 	BeginBatchDraw();
 
 	while (running)
 	{
+		DWORD start_time = GetTickCount();
 		while (peekmessage(&msg))
 		{
-
+			
 		}
 
 		static int counter = 0;
+		if (++counter % 5 == 0) idx_cur_anim++;
+		idx_cur_anim %= ANIM_NUM;
 
 		cleardevice();
+
 		putimage(0, 0, &img_background);
+		putimage(500, 500, &img_player_right[idx_cur_anim]);
+
 		FlushBatchDraw();
+
+		DWORD end_time = GetTickCount();
+		DWORD delta_time = end_time - start_time;
+        if (delta_time < 1000/180) Sleep(1000/180 - delta_time);
 	}
 
 	EndBatchDraw();
